2840697f491275a2906f17a5cf9390d5ec0f6b65
DwarakJ <59662348+DwarakJ@users.noreply.github.com>
2023-01-16 13:31:29 +0000

    Changed

--- create_python_env.sh
+++ create_python_env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-virtualenv -p python3.8 ENV
-./ENV/bin/pip3 install pip3 --upgrade
-./ENV/bin/pip3 install --use-deprecated=legacy-resolver -r requirements.txt
+virtualenv -p python3.8 ENV2
+#./ENV/bin/pip3 install pip3 --upgrade
+#./ENV/bin/pip3 install --use-deprecated=legacy-resolver -r requirements.txt
+pip install boto3
